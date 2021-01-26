e4972962f10ed115fcb9a35a44aab0bdb5d6372e
Luis Quiroz <lquiroz@dt-data.com>
2021-01-26 11:37:02 -0500

    Update

--- heroku_update.bat
+++ heroku_update.bat
@@ -4,6 +4,6 @@ manage.py collectstatic
 pip freeze>requirements.txt
 git add .
 git commit -m "Update"
-git push origin main
-git push heroku main
+git push origin master
+git push heroku master
 heroku run python manage.py migrate -a dt-data-pea
