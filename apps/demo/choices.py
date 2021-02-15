8b60acb6a2ed830dbef88a4ba49b8fd8ffacae37
Luis Quiroz <lquiroz@dt-data.com>
2021-02-15 15:36:18 -0500

    Update

--- apps/demo/choices.py
+++ apps/demo/choices.py
@@ -40,6 +40,12 @@ INCOME_STATEMENT = {
 		('4', 'Sales & Marketing'),
 	)
 }
+EBITDA = [
+	('1', 'Other Non Operating Income/Expense'),
+	('2', 'Interest Expense/Income'),
+	('3', 'Depreciation and Amortization'),
+	('4', 'Foreign Currency Translation'),
+]
 
 
 def rint(m,n):
@@ -47,6 +53,40 @@ def rint(m,n):
 
 def rfloat(m,n):
 	return round(random.randint(m,n) + random.random(), 2)
+
+def get_ebitda_data(ebitda):
+	for i in EBITDA:
+		if ebitda == EBITDA[0][1]:
+			return 300
+		elif ebitda == EBITDA[1][1]:
+			return 200
+		elif ebitda == EBITDA[2][1]:
+			return 1500
+		elif ebitda == EBITDA[3][1]:
+			return 200
+		else:
+			return -1
+
+def get_last_totals():
+	totals = []
+	for c in CATEGORIES:
+		for d in INCOME_STATEMENT[c[1]]:
+			totals.append({
+				'category': c[1],
+				'item': d[1],
+				'data': rint(10000, 100000)
+			})
+	return totals
+
+def get_last_ebdita_totals():
+	totals = {}
+	for e in EBITDA:
+		totals[e[1]] = rint(-5000, 5000)
+	totals['income'] = rint(-20000, 20000)
+	totals['iva'] = rint(-40000, 40000)
+	totals['rent'] = rint(-40000, 40000)
+	totals['net'] = rint(-200000, 200000)
+	return totals
 
 def get_pea():
 	report = []
@@ -63,11 +103,43 @@ def get_pea():
 						'data': data,
 					}
 				)
+	ebitda = []
+	for e in EBITDA:
+		for m in MONTHS:
+			ebitda.append({
+				'month': m[1],
+				'ebitda': e[1],
+				'data': get_ebitda_data(e[1])
+			})
+	IVA = []
+	TOTAL_IVA = 0
+	for m in MONTHS:
+		IVA.append({
+			'month': m[1],
+			'data': 3026.36833333333,	
+		})
+		TOTAL_IVA += 3026.36833333333
+	RENT = []
+	TOTAL_RENT = 0
+	for m in MONTHS:
+		RENT.append({
+			'month': m[1],
+			'data': 4539.5525,	
+		})
+		TOTAL_RENT += 4539.5525
 	context = {
 		'report':report,
 		'categories': [ i[1] for i in CATEGORIES ],
 		'months': [ m[1] for m in MONTHS ],
-		# 'income_statement': [ ins[1] for ins in INCOME_STATEMENT.items()],
 		'income_statement': INCOME_STATEMENT,
+		'ebitda_names': [ e[1] for e in EBITDA ],
+		'ebitda': ebitda,
+		'iva': IVA,
+		'total_iva': int(TOTAL_IVA),
+		'rent': RENT,
+		'total_rent': int(TOTAL_RENT),
+		'last_totals': get_last_totals(),
+		'last_ebdita': rint(-50000,50000),
+		'last_ebdita_totals': get_last_ebdita_totals(),
 	}
 	return context
--- apps/demo/templatetags/demo_tags.py
+++ apps/demo/templatetags/demo_tags.py
@@ -1,29 +1,17 @@
 from django import template
+from apps.demo.choices import (EBITDA,)
 
 register = template.Library()
 
-@register.simple_tag()
-def get_data(report, month, category, item):
-	for i in report:
-		if i['month'] == month and i['category'] == category and i['item'] == item:
-			# return i['data']
-			return "{:,}".format(i['data'])
-	return 'None'
+@register.filter()
+def key(dictionary, key):
+	return dictionary[key]
 
-@register.simple_tag()
-def get_total_month(report, month, category):
-	total = 0
-	for i in report:
-		if i['month'] == month and i['category'] == category:
-			total = total + i['data']
-	# return round(total, 2)
-	return "{:,}".format(total)
+@register.filter()
+def coma(value):
+	return "{:,}".format(value)
 
-
-@register.simple_tag()
-def get_operating_margin_class(report, month):
-	value = get_operating_margin(report, month)
-	value = float(value.replace(',',''))
+def value_color(value):
 	if value == 0:
 		return 'warning'
 	elif value > 0:
@@ -32,24 +20,7 @@ def get_operating_margin_class(report, month):
 		return 'danger'
 
 @register.simple_tag()
-def get_total_income_statement(report, item):
-	total = 0
-	for i in report:
-		if i['item'] == item:
-			total = total + i['data']
-	return "{:,}".format(total)
-
-@register.simple_tag()
-def get_total_category(report, category):
-	total = 0
-	for i in report:
-		if i['category'] == category:
-			total = total + i['data']
-	return "{:,}".format(total)
-
-
-@register.simple_tag()
-def get_operating_margin(report, month):
+def get_operating_margin(report, month, coma_format=True):
 	t_revenues = 0
 	t_cogs = 0
 	t_expenses = 0
@@ -60,12 +31,17 @@ def get_operating_margin(report, month):
 			t_cogs = t_cogs + i['data']
 		if i['month'] == month and i['category'] == 'OPERATING EXPENSES':
 			t_expenses = t_expenses + i['data']
-	# return round(total, 2)
-	# return "{:,}".format(t_revenues - t_cogs - t_expenses)
-	return "{:,}".format(t_revenues - t_cogs - t_expenses)
+	if coma_format:
+		return coma(t_revenues - t_cogs - t_expenses)
+	else:
+		return t_revenues - t_cogs - t_expenses
 
 @register.simple_tag()
-def get_total_operating_margin(report):
+def get_operating_margin_class(report, month):
+	return value_color(get_operating_margin(report, month, False))
+
+@register.simple_tag()
+def get_total_operating_margin(report, coma_format=True):
 	t_revenues = 0
 	t_cogs = 0
 	t_expenses = 0
@@ -76,14 +52,261 @@ def get_total_operating_margin(report):
 			t_cogs = t_cogs + i['data']
 		if i['category'] == 'OPERATING EXPENSES':
 			t_expenses = t_expenses + i['data']
-	# return round(total, 2)
-	# return "{:,}".format(t_revenues - t_cogs - t_expenses)
-	return "{:,}".format(t_revenues - t_cogs - t_expenses)
+	if coma_format:
+		return coma(t_revenues - t_cogs - t_expenses)
+	return t_revenues - t_cogs - t_expenses
 
-@register.filter()
-def key(dictionary, key):
-	return dictionary[key]
+@register.simple_tag()
+def get_data(report, month, category, item):
+	for i in report:
+		if i['month'] == month and i['category'] == category and i['item'] == item:
+			return coma(i['data'])
+	return 'None'
 
-@register.filter()
-def floatcoma(value):
-	return "{:,}".format(value)
+@register.simple_tag()
+def get_ebitda_item(ebitda_report, month, ebitda):
+	for i in ebitda_report:
+		if i['month'] == month and i['ebitda'] == ebitda:
+			return coma(i['data'])
+	return 'None'
+
+@register.simple_tag()
+def get_ebitda(report, ebitda_report, month, coma_format=True):
+	value = get_operating_margin(report, month, False)
+	for i in report:
+		for e in ebitda_report:
+			if i['month'] == month and e['ebitda'] == 'Depreciation and Amortization':
+				if coma_format:
+					return coma(value + e['data'])
+				else:
+					return value + e['data']
+	raise Exception
+
+@register.simple_tag()
+def get_ebitda_class(report, ebitda_report, month):
+	return value_color(get_ebitda(report, ebitda_report, month, False))
+
+@register.simple_tag()
+def get_total_ebitda(report, ebitda_report, months, coma_format=True):
+	total = 0
+	for month in months:
+		total += get_ebitda(report, ebitda_report, month, False)
+	if coma_format:
+		return coma(total)
+	return total
+
+@register.simple_tag()
+def get_total_ebitda_item(ebitda_report, ebitda, coma_format=False):
+	total = 0
+	for i in ebitda_report:
+		if i['ebitda'] == ebitda:
+			total += i['data']
+	if coma_format:
+		return coma(total)
+	return total
+
+def get_month_ebdita(ebitda_report, month):
+	total = 0
+	for e in ebitda_report:
+		if e['month'] == month:
+			total += e['data']
+	return total
+
+@register.simple_tag()
+def get_income_before_tax(report, ebitda_report, month, coma_format=True):
+	ebitda = get_ebitda(report, ebitda_report, month, False)
+	month_sum = get_month_ebdita(ebitda_report, month)
+	if coma_format:
+		return coma(ebitda - month_sum)
+	return ebitda - month_sum
+
+@register.simple_tag()
+def get_total_income_before_tax(report, ebitda_report, months, coma_format=True):
+	total = 0
+	for month in months:
+		total += get_income_before_tax(report, ebitda_report, month, False)
+	if coma_format:
+		return coma(total)
+	return total
+
+@register.simple_tag()
+def get_total_month(report, month, category):
+	total = 0
+	for i in report:
+		if i['month'] == month and i['category'] == category:
+			total = total + i['data']
+	return coma(total)
+
+@register.simple_tag()
+def get_total_income_statement(report, item, coma_format=True):
+	total = 0
+	for i in report:
+		if i['item'] == item:
+			total = total + i['data']
+	if coma_format:
+		return coma(total)
+	return total
+
+@register.simple_tag()
+def get_total_category(report, category, coma_format=True):
+	total = 0
+	for i in report:
+		if i['category'] == category:
+			total = total + i['data']
+	if coma_format:
+		return coma(total)
+	return total
+
+@register.simple_tag()
+def get_data_by_moth(data:list, month, coma_format=True):
+	for d in data:
+		if d['month'] == month:
+			value = int(d['data'])
+			if coma_format:
+				return (coma(value))
+			return value
+
+@register.simple_tag()
+def get_income(report, ebitda_report, iva, rent, month, coma_format=True):
+	income = get_income_before_tax(report, ebitda_report, month, False)
+	iva_value = get_data_by_moth(iva, month, False)
+	rent_value = get_data_by_moth(rent, month, False)
+	if coma_format:
+		return coma(income - iva_value - rent_value)
+	return income - iva_value - rent_value
+
+@register.simple_tag()
+def get_total_income(report, ebitda_report, iva, rent, months, coma_format=True):
+	total = 0
+	for month in months:
+		total += get_income(report, ebitda_report, iva, rent, month, False)
+	if coma_format:
+		return coma(total)
+	return total
+
+@register.simple_tag()
+def get_income_class(report, ebitda_report, iva, rent, month):
+	return value_color(get_income(report, ebitda_report, iva, rent, month, False))
+	
+@register.simple_tag()
+def get_last_totals(last_totals, item, coma_format=True):
+	for lt in last_totals:
+		if item == lt['item']:
+			if coma_format:
+				return coma(lt['data'])
+			return lt['data']
+
+@register.simple_tag()
+def get_last_totals_category(last_totals, category, coma_format=True):
+	total = 0
+	for lt in last_totals:
+		if category == lt['category']:
+			total += lt['data']	
+	if coma_format:
+		return coma(total)
+	return total
+
+@register.simple_tag()
+def get_last_totals_operating_margin(last_totals):
+	t_revenues = 0
+	t_cogs = 0
+	t_expenses = 0
+	for i in last_totals:
+		if i['category'] == 'REVENUES':
+			t_revenues = t_revenues + i['data']
+		if i['category'] == 'COGS':
+			t_cogs = t_cogs + i['data']
+		if i['category'] == 'OPERATING EXPENSES':
+			t_expenses = t_expenses + i['data']
+	return coma(t_revenues - t_cogs - t_expenses)
+
+@register.simple_tag()
+def get_var_income_statement(report, last_totals, item, coma_format=True):
+	total_income = get_total_income_statement(report, item, False)
+	last_total_income = get_last_totals(last_totals, item, False)
+	if coma_format:
+		return coma(total_income - last_total_income)
+	return total_income - last_total_income
+
+@register.simple_tag()
+def get_var_income_statement_class(report, last_totals, item):
+	return value_color(
+		get_var_income_statement(report, last_totals, item, False))
+
+@register.simple_tag()
+def get_var_total_category(report, category, last_totals, coma_format=True):
+	total_income = get_total_category(report, category, False)
+	last_total_income = get_last_totals_category(last_totals, category, False)
+	if coma_format:
+		return coma(total_income - last_total_income)
+	return total_income - last_total_income
+
+@register.simple_tag()
+def get_var_total_category_class(report, category, last_totals):
+	return value_color(
+		get_var_total_category(report, category, last_totals, False))
+
+@register.simple_tag()
+def get_var_last_totals_operating_margin(report, last_totals, coma_format=True):
+	total_operating_margin = get_total_operating_margin(report, False)
+	last_total_operating_margin = get_total_operating_margin(last_totals, False)
+	if coma_format:
+		return coma(total_operating_margin - last_total_operating_margin)
+	return total_operating_margin - last_total_operating_margin
+
+@register.simple_tag()
+def get_var_last_totals_operating_margin_class(report, last_totals):
+	return value_color(
+		get_var_last_totals_operating_margin(report, last_totals, False))
+
+@register.simple_tag()
+def get_var_total_ebitda(report, ebitda_report, months, last_ebdita, coma_format=True):
+	ebitda = get_total_ebitda(report, ebitda_report, months, False)
+	if coma_format:
+		return coma(ebitda - last_ebdita)
+	return ebitda - last_ebdita
+
+@register.simple_tag()
+def get_var_total_ebitda_class(report, ebitda_report, months, last_ebdita, coma_format=True):
+	return value_color(
+		get_var_total_ebitda(report, ebitda_report, months, last_ebdita, False))
+	
+@register.simple_tag()
+def get_var_total_ebitda_item(ebitda_report, last_ebdita_totals, item, coma_format=True):
+	total_ebitda = get_total_ebitda_item(ebitda_report, item, False)
+	if coma_format:
+		return coma(total_ebitda - last_ebdita_totals[item])
+	return total_ebitda - last_ebdita_totals[item]
+
+@register.simple_tag()
+def get_var_total_ebitda_item_class(ebitda_report, last_ebdita_totals, item):
+	return value_color(
+		get_var_total_ebitda_item(ebitda_report, last_ebdita_totals, item, False))
+
+@register.simple_tag()
+def get_var_total_income_before_tax(report, ebitda_report, months, last_ebdita_totals, coma_format=True):
+	total_income_before_tax = get_total_income_before_tax(report, ebitda_report, months, False)
+	last_total = last_ebdita_totals['income']
+	if coma_format:
+		return coma(total_income_before_tax - last_total)
+	return total_income_before_tax - last_total
+
+@register.simple_tag()
+def get_var_total_income_before_tax_class(report, ebitda_report, months, last_ebdita_totals):
+	return value_color(
+		get_var_total_income_before_tax(report, ebitda_report, months, last_ebdita_totals, False))
+
+@register.simple_tag()
+def get_var_total_income(report, ebitda_report, iva, rent, months, last_ebdita_totals, coma_format=True):
+	total_income = get_total_income(report, ebitda_report, iva, rent, months, False)
+	if coma_format:
+		return coma(total_income - last_ebdita_totals['net'])
+	return total_income - last_ebdita_totals['net']
+
+@register.simple_tag()
+def substraction(value1, value2):
+	return coma(value1 - value2)
+
+@register.simple_tag()
+def substraction_class(value1, value2):
+	return value_color(value1 - value2)
